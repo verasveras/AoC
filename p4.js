e363a12cbc8b5e8f87aeec6ed6efecccb352f7da
verasveras <lisaveras@gmail.com>
2016-12-10 15:26:45 -0500

    Code clean up

--- p4.js
+++ p4.js
@@ -1,10 +1,5 @@
 const input = require('./p4input');
-
 let sum = 0;
-
-function isRoomValid(roomNumber){
-
-}
 
 for (let i = 0; i < input.length; i++){
 	
@@ -13,7 +8,7 @@ for (let i = 0; i < input.length; i++){
 	let room = input[i].split('-');
 	let last = room.length - 1;
 	let letters = room.slice(0, last).join('');
-		letters = letters.split('').sort().join('');
+	letters = letters.split('').sort().join('');
 	let sector = room[last].match(/\d+/)[0];
 	let checksum = room[last].match(/[a-z]+/)[0];
 	
@@ -21,8 +16,8 @@ for (let i = 0; i < input.length; i++){
 	// get the frequency count of all the letters
 	// compressedLetters is all the letters that show up
 	// compressedCount is the # of times a letter at the same index in compressed letters appears
-	let compressedLetters = '';
-	let compressedCount = '';
+	let compressedLetters = [];
+	let compressedCount = [];
 	let count = 1;
 
 	let letter = letters[0];
@@ -30,17 +25,15 @@ for (let i = 0; i < input.length; i++){
 		if (letters[j] == letter)
 			count++;
 		else {
-			compressedLetters += letter;
-			compressedCount += count;
+			compressedLetters.push(letter);
+			compressedCount.push(count);
 			count = 1;
 			letter = letters[j];
 		}
 	}
 
-	compressedLetters = compressedLetters.split('');
-	compressedCount = compressedCount.split('');
-	compressedCount = compressedCount.map((element) => Number(element));
-
+	compressedLetters.push(letter);
+	compressedCount.push(count);
 
 	// calculate the actualChecksum of the characters
 	let actualCheckSum = [];
@@ -52,16 +45,11 @@ for (let i = 0; i < input.length; i++){
 		compressedCount[index] = 0;
 	}
 
-	actualCheckSum = actualCheckSum.sort().join('');
-	// console.log('Actual checksum: ', actualCheckSum);
-	// console.log('Given checksum: ', checksum);
-
+	actualCheckSum = actualCheckSum.join('');
 
 	// compare the actual checksum and the given checksum
 	if (actualCheckSum == checksum) {
-		console.log(room);
 		sum += Number(sector);
-		console.log('added!', sum);
 	}
 
 }
